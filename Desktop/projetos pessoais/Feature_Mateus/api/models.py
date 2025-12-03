1ae48f32d2046765a172e8cf5232e72eabda444f
Mateus Andrade <mateusandrade092@gmail.com>
2025-12-03 15:16:37 -0300

    feat(api): criação de  modelos Categoria e Produto e aplicação das  migrações iniciais

---
+++ Desktop/projetos pessoais/Feature_Mateus/api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+    
+
+class Produto(models.Model):
+    nome= models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10,decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nome
