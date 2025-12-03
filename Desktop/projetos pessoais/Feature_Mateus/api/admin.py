57494466def2ffecccdd717d4084855d12c19bcb
Mateus Andrade <mateusandrade092@gmail.com>
2025-12-03 15:51:53 -0300

    Registro de categoria e produto no admin.py

--- Desktop/projetos pessoais/Feature_Mateus/api/admin.py
+++ Desktop/projetos pessoais/Feature_Mateus/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from django.contrib import admin
+from .models import Categoria, Produto
 # Register your models here.
+
+
+admin.site.register(Categoria)
+admin.site.register(Produto)
+
+
